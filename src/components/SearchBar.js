ef1c8636e9c91822a9b5c6384ce49b9d06922fc1
jabmat <mjablonski.praca@gmail.com>
2024-01-13 14:23:44 +0100

    addeded frist components

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+const SearchBar = () => {
+	return (
+		<>
+			<div>
+				<p>SearchBar:</p>
+                <form>
+                    <label for='search-song'>Search song: </label>
+					<input type='search' id='search-song'></input>
+				    <button>Search</button>
+				</form>
+			</div>
+		</>
+	);
+};
+
+export default SearchBar;
---
+++ src/components/Track.js
@@ -0,0 +1,11 @@
+const Track = () => {
+    return (
+        <>
+            <div>
+                <p>Track</p>
+            </div>
+        </>
+    )
+}
+
+export default Track
